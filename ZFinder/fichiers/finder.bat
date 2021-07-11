f7f15c8f5049eb64f61e933ec73cf380a2cee051
Pixel. YO <83309151+pixel-yo@users.noreply.github.com>
2021-07-11 20:33:25 +0200

    Update finder.bat

--- ZFinder/fichiers/finder.bat
+++ ZFinder/fichiers/finder.bat
@@ -6,11 +6,13 @@ title [BETA] WEBHOOK TOOL BY ZELRON - https://discord.gg/kmCJ25nPcY
 
 pause
 
+set /p ext="[>] Quel est le type de fichier ? (.exe ou .py) : "
+
 set /p file="[>] Saisir le nom du fichier a examiner : "
 
 @echo.
 
-findstr https://discord.com/api/webhooks ./%file%
+findstr https://discord.com/api/webhooks ./%file%%ext%
 
 @echo.
 @echo "[>] Pour detruire le webhook, veuillez lancer le launcher avec 'b' et selectionner la premiere option"
@@ -30,10 +32,11 @@ set c="[c] Quitter"
 set /p del="[>] Que voulez vous faire ?" 
 
 if %del% == a (
-@echo https://discord.gg/kmCJ25nPcY )
+start discord.url )
 
 if %del% == b (
 start launcher.lnk )
 
 if %del% == c (
 stop )
+
